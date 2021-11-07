177e64026ad63b25055ffb037e037e286023aacd
Swagata Chanda <swagata.chanda2000@gmail.com>
2021-11-07 23:24:26 +0530

    Get data added

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -5,5 +5,6 @@ const dataController = require('../controller/dataController')
 router.use(express.json());
 
 router.post("/", dataController.addData);
+router.get("/", dataController.getData)
 
 module.exports = router;
