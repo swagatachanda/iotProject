8b7e06c0d21a65bdda8e3ef91f0dcdb2c03f4b7d
Ayantika Bhaumik <ayantikabhaumik@gmail.com>
2021-11-07 11:24:38 +0530

    Mondel added

---
+++ models/data.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const data = mongoose.Schema({
+    timestamp:{
+        type:Date,
+        default:new Date(),
+        required: true
+    },
+
+    data:{
+        type:Number,
+        required:true
+    },
+})
+module.exports = mongoose.model("data",data)
